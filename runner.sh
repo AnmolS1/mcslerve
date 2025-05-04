5d20181163af1b4ea8a44b416cb427b06f6f5f09
Anmol <anmolsaxena456@gmail.com>
2025-05-04 18:17:58 -0500

    session at 2025-05-04 18:17:46

--- runner.sh
+++ runner.sh
@@ -2,10 +2,10 @@
 
 git pull
 
+printf -v committer 'session at %(%Y-%m-%d %H:%M:%S)T\n' -1
+
 java -Xmx4G -jar fabric-server-launch.jar nogui
 
-printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1
-
 git add .
-git commit -m "$date"
+git commit -m "$committer"
 git push
