e593e0c215597629f3d85bad3731ae054d6239aa
Anmol <anmolsaxena456@gmail.com>
2025-05-04 18:27:53 -0500

    session on May 04 2025 at 18:27:24

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 
 git pull
 
-printf -v committer 'session on %(%^b %d %Y at %H:%M:%S)T\n' -1
+printf -v committer 'session on %(%b %d %Y at %H:%M:%S)T\n' -1
 
 java -Xmx4G -jar fabric-server-launch.jar nogui
 
