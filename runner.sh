a5c29bdd84ac14b4315d310d3ae6c85916d2cdcf
diya <saxenadiya07@gmail.com>
2025-05-04 22:50:25 -0500

    Revert "ion on"
    This reverts commit fcb2baf43f863262e629fca2bdd74b99ba3df581.

---
+++ runner.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git pull
+
+printf -v committer 'session on %(%b %d %Y at %I:%M:%S %p)T\n' -1
+
+cd ~/GitHub/mcslerve
+
+java -Xmx4G -jar fabric-server-launch.jar nogui
+
+git add .
+git commit -m "$committer"
+git push
